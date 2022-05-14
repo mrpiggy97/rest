42b5b420330d5ba3e20fd2bb9b5afb47cd2091b6
mrpiggy97 <fabyjesusrivas10@gmail.com>
2022-05-14 02:40:43 +0000

    changed structure of response

--- handlers/Home.go
+++ handlers/Home.go
@@ -8,17 +8,21 @@ import (
 )
 
 type HomeResponse struct {
-	Message string `json:"message"`
-	Status  bool   `json:"status"`
+	Message           string `json:"message"`
+	Status            bool   `json:"status"`
+	UserAuthenticated bool   `json:"userAuthenticated"`
 }
 
 func HomeHandler(s server.IServer) http.HandlerFunc {
 	return func(writer http.ResponseWriter, req *http.Request) {
 		writer.Header().Add("Content-type", "application/json")
-		writer.WriteHeader(http.StatusOK)
+		var key server.Key = "isAuthenticated"
+		var value interface{} = req.Context().Value(key)
+		var boolValue bool = value.(bool)
 		json.NewEncoder(writer).Encode(HomeResponse{
-			Message: "welcome to the api",
-			Status:  true,
+			Message:           "welcome to the api",
+			Status:            true,
+			UserAuthenticated: boolValue,
 		})
 	}
 }
