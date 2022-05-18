b94e8c172fbe8293bb6e2a8042a1bb48ec2bbf75
mrpiggy97 <fabyjesusrivas10@gmail.com>
2022-05-18 23:37:51 +0000

    deleted Start method, simplified ways to get Server fields

--- server/Server.go
+++ server/Server.go
@@ -3,40 +3,26 @@ package server
 import (
 	"context"
 	"errors"
-	"fmt"
-	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
-	"github.com/mrpiggy97/rest/database"
-	"github.com/mrpiggy97/rest/repository"
+	"github.com/mrpiggy97/rest/middleware"
 )
 
-type Config struct {
-	Port        string
-	JWTSecret   string
-	DatabaseUrl string
-}
-
 type IServer interface {
-	Config() *Config
 	ServeHTTP(writer http.ResponseWriter, req *http.Request)
 }
 
-type Broker struct {
-	config          *Config
-	router          *mux.Router
-	middlewareFuncs []MiddlewareFunc
+type Server struct {
+	Config          *Config
+	Router          *mux.Router
+	MiddlewareFuncs []middleware.MiddlewareFunc
 }
 
-func (broker *Broker) Config() *Config {
-	return broker.config
-}
-
-func (broker *Broker) ServeHTTP(writer http.ResponseWriter, req *http.Request) {
-	var middlewareResponse MiddlewareResponse
-	for _, middlewareFunc := range broker.middlewareFuncs {
-		middlewareResponse = middlewareFunc(broker, writer, req)
+func (server *Server) ServeHTTP(writer http.ResponseWriter, req *http.Request) {
+	var middlewareResponse middleware.MiddlewareResponse
+	for _, middlewareFunc := range server.MiddlewareFuncs {
+		middlewareResponse = middlewareFunc(writer, req)
 		req = middlewareResponse.Request
 		if middlewareResponse.Err != nil {
 			break
@@ -45,38 +31,24 @@ func (broker *Broker) ServeHTTP(writer http.ResponseWriter, req *http.Request) {
 	if middlewareResponse.Err != nil {
 		http.Error(writer, middlewareResponse.Err.Error(), middlewareResponse.StatusCode)
 	} else {
-		broker.router.ServeHTTP(writer, req)
+		server.Router.ServeHTTP(writer, req)
 	}
 }
 
-func (broker *Broker) Start(binder func(appServer IServer, router *mux.Router)) {
-	broker.router = mux.NewRouter()
-	binder(broker, broker.router)
-	repo, repoErr := database.NewPostgresqlRespository(broker.config.DatabaseUrl)
-	if repoErr != nil {
-		log.Fatal(repoErr.Error())
+func NewServer(cxt context.Context) (*Server, error) {
+	var server *Server = &Server{
+		Config:          GetConfig(),
+		MiddlewareFuncs: GetMiddlewareFuncs(),
+		Router:          GetRouter(),
 	}
-	repository.SetRepository(repo)
-	fmt.Println("starting server at port ", broker.config.Port)
-	if err := http.ListenAndServe(broker.config.Port, broker); err != nil {
-		log.Fatal(err.Error())
-	}
-}
-
-func NewServer(cxt context.Context, config *Config) (*Broker, error) {
-	if config.Port == "" {
+	if server.Config.Port == "" {
 		return nil, errors.New("port must not be empty")
 	}
-	if config.JWTSecret == "" {
+	if server.Config.JWTSecret == "" {
 		return nil, errors.New("jwtSecret must not be empty")
 	}
-	if config.DatabaseUrl == "" {
+	if server.Config.DatabaseUrl == "" {
 		return nil, errors.New("databaseUrl must not be empty")
 	}
-	var broker *Broker = &Broker{
-		config:          config,
-		router:          mux.NewRouter(),
-		middlewareFuncs: []MiddlewareFunc{RequestIsAuthenticated, AuthorizationMiddleware},
-	}
-	return broker, nil
+	return server, nil
 }
