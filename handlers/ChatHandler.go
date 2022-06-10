7a9aff92db38702da54e3226ca09c4692ad7614b
mrpiggy97 <fabyjesusrivas10@gmail.com>
2022-06-10 14:57:19 +0000

    updated response type to return

--- handlers/ChatHandler.go
+++ handlers/ChatHandler.go
@@ -5,14 +5,25 @@ import (
 	"net/http"
 
 	"github.com/mrpiggy97/rest/repository"
+	"github.com/segmentio/ksuid"
 )
 
 type ChatHandlerRequest struct {
 	Message string `json:"message"`
 }
 
+type ChatHandlerResponse struct {
+	Message string `json:"message"`
+	UUID    string `json:"uuid"`
+}
+
 func ChatHandler(writer http.ResponseWriter, req *http.Request) {
-	var message *ChatHandlerRequest = new(ChatHandlerRequest)
-	json.NewDecoder(req.Body).Decode(message)
-	repository.AppHub.BroadCast(message, nil)
+	var request *ChatHandlerRequest = new(ChatHandlerRequest)
+	json.NewDecoder(req.Body).Decode(request)
+	var uuid string = ksuid.New().String()
+	var message *ChatHandlerResponse = &ChatHandlerResponse{
+		Message: request.Message,
+		UUID:    uuid,
+	}
+	repository.AppHub.BroadCast(message, "")
 }
