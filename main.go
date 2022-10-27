280bded9dbc595d2cf8a75768edd2119cf22edbe
mrpiggy97 <fabyjesusrivas10@gmail.com>
2022-10-26 23:02:34 -0400

    deleted env variable loading from .env file

--- main.go
+++ main.go
@@ -1,17 +1,10 @@
 package main
 
 import (
-	"log"
-
-	"github.com/joho/godotenv"
 	"github.com/mrpiggy97/rest/server"
 )
 
 func main() {
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatal(err.Error())
-	}
 
 	server.Runserver()
 }
