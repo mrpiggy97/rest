2e873b3d0ef1afa535d14cc6d323e70b727f132e
mrpiggy97 <fabyjesusrivas10@gmail.com>
2022-11-25 15:26:04 +0000

    added TestMain function to set up testing server for hanlders test

--- handlers/handlers_test.go
+++ handlers/handlers_test.go
@@ -1,8 +1,18 @@
 package handlers_test
 
-import "testing"
+import (
+	"os"
+	"testing"
+
+	"github.com/mrpiggy97/rest/server"
+)
 
 func TestHandlers(testCase *testing.T) {
 	testCase.Run("action=test-home", testHome)
 	testCase.Run("action=test-signup-handler", testSignUpHandler)
+}
+
+func TestMain(m *testing.M) {
+	go server.Runserver(server.GetTestingConfig())
+	os.Exit(m.Run())
 }
